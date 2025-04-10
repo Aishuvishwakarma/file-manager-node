17f5aae965a0679b0a3425287ab223644be17f47
Ashish Vishwakarma <59652604+Aishuvishwakarma@users.noreply.github.com>
2025-04-10 18:20:07 +0530

    build uploaded

--- dist/controllers/file.controller.js
+++ dist/controllers/file.controller.js
@@ -8,9 +8,14 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getFileById = exports.getFiles = void 0;
+exports.deleteFile = exports.getFileById = exports.getFiles = void 0;
 const file_model_1 = require("../models/file.model");
+const promises_1 = __importDefault(require("fs/promises"));
+const path_1 = __importDefault(require("path"));
 const getFiles = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { folderId, page = 1, limit = 10 } = req.query;
     const query = folderId ? { folder: folderId } : {};
@@ -30,3 +35,28 @@ const getFileById = (req, res) => __awaiter(void 0, void 0, void 0, function* ()
     res.status(200).json(file);
 });
 exports.getFileById = getFileById;
+const deleteFile = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const _id = req.params.id;
+        const file = yield file_model_1.File.findById(_id);
+        if (!file) {
+            res.status(404).json({ error: "File not found in DB" });
+        }
+        console.log(file.path);
+        const filePath = path_1.default.join(__dirname, "..", "..", file.path);
+        console.log(filePath);
+        try {
+            yield promises_1.default.unlink(filePath);
+        }
+        catch (fsError) {
+            console.warn(`Could not delete physical file: ${fsError}`);
+        }
+        yield file_model_1.File.deleteOne({ _id });
+        res.status(200).json({ message: "File deleted successfully" });
+    }
+    catch (error) {
+        console.error("Error deleting file:", error);
+        res.status(500).json({ error: "Failed to delete file" });
+    }
+});
+exports.deleteFile = deleteFile;
--- dist/controllers/folder.controller.js
+++ dist/controllers/folder.controller.js
@@ -9,7 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.searchFoldersAndFiles = exports.getFolderStructure = exports.createFolder = void 0;
+exports.deleteFolder = exports.updateFolder = exports.getFolderStructure = exports.createFolder = void 0;
 const folder_model_1 = require("../models/folder.model");
 const file_model_1 = require("../models/file.model");
 const createFolder = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -19,21 +19,91 @@ const createFolder = (req, res) => __awaiter(void 0, void 0, void 0, function* (
     res.status(201).json(folder);
 });
 exports.createFolder = createFolder;
+// Recursive
+const buildFolderTree = (...args_1) => __awaiter(void 0, [...args_1], void 0, function* (parentId = null, filters = {}) {
+    const folderQuery = { parent: parentId };
+    if (filters.name) {
+        folderQuery.name = { $regex: filters.name, $options: "i" };
+    }
+    if (filters.description) {
+        folderQuery.description = { $regex: filters.description, $options: "i" };
+    }
+    if (filters.createdAt) {
+        const date = new Date(filters.createdAt);
+        const nextDay = new Date(date);
+        nextDay.setDate(date.getDate() + 1);
+        folderQuery.createdAt = { $gte: date, $lt: nextDay };
+    }
+    const folders = yield folder_model_1.Folder.find(folderQuery).lean();
+    const foldersWithChildrenAndFiles = yield Promise.all(folders.map((folder) => __awaiter(void 0, void 0, void 0, function* () {
+        const children = yield buildFolderTree(folder._id.toString(), filters);
+        const fileQuery = { folder: folder._id };
+        if (filters.name) {
+            fileQuery.name = { $regex: filters.name, $options: "i" };
+        }
+        if (filters.createdAt) {
+            const date = new Date(filters.createdAt);
+            const nextDay = new Date(date);
+            nextDay.setDate(date.getDate() + 1);
+            fileQuery.createdAt = { $gte: date, $lt: nextDay };
+        }
+        const files = yield file_model_1.File.find(fileQuery).lean();
+        return Object.assign(Object.assign({}, folder), { children,
+            files });
+    })));
+    return foldersWithChildrenAndFiles;
+});
 const getFolderStructure = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const folders = yield folder_model_1.Folder.find();
-    res.json({ folders });
+    var _a, _b, _c;
+    try {
+        const filters = {
+            name: (_a = req.query.name) === null || _a === void 0 ? void 0 : _a.toString(),
+            description: (_b = req.query.description) === null || _b === void 0 ? void 0 : _b.toString(),
+            createdAt: (_c = req.query.createdAt) === null || _c === void 0 ? void 0 : _c.toString(),
+        };
+        const folders = yield buildFolderTree(null, filters);
+        const files = yield file_model_1.File.find({ folder: null }).lean();
+        res.json({ folders, files });
+    }
+    catch (error) {
+        console.error("Error building folder structure:", error);
+        res.status(500).json({ error: "Failed to fetch folder structure" });
+    }
 });
 exports.getFolderStructure = getFolderStructure;
-// export const getFolderStructure = async (req: Request, res: Response) => {
-//   const folders = await Folder.find();
-//   const files = await File.find();
-//   res.json({ folders, files });
-// };
-const searchFoldersAndFiles = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { term, folderId } = req.query;
-    const regex = new RegExp(term, "i");
-    const folders = yield folder_model_1.Folder.find({ parent: folderId, name: regex });
-    const files = yield file_model_1.File.find({ folder: folderId, name: regex });
-    res.json({ folders, files });
+const updateFolder = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const _id = req.params.id;
+        const data = req.body;
+        const updatedFolder = yield folder_model_1.Folder.findByIdAndUpdate(_id, data, {
+            new: true,
+        });
+        if (!updatedFolder) {
+            res.status(404).json({ error: "Folder not found" });
+            return;
+        }
+        res.status(200).json({ message: "Folder updated", folder: updatedFolder });
+    }
+    catch (error) {
+        console.error("Error updating folder:", error);
+        res.status(500).json({ error: "Failed to update folder" });
+    }
 });
-exports.searchFoldersAndFiles = searchFoldersAndFiles;
+exports.updateFolder = updateFolder;
+const deleteFolder = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const _id = req.params.id;
+        const result = yield folder_model_1.Folder.deleteOne({ _id });
+        yield folder_model_1.Folder.deleteOne({ parent: _id });
+        if (result.deletedCount === 0) {
+            res.status(404).json({ error: "Folder not found" });
+            return;
+        }
+        res.status(200).json({ message: "Folder deleted successfully" });
+    }
+    catch (error) {
+        console.error("Error deleting folder:", error);
+        res.status(500).json({ error: "Failed to delete folder" });
+    }
+});
+exports.deleteFolder = deleteFolder;
